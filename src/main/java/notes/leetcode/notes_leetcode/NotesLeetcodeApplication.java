0ac0943c63412640ce383ac4e4b11c1c776ab763
Karthee <kartheeganapathi@gmail.com>
2025-05-13 19:49:16 +0530

    initial commit

---
+++ src/main/java/notes/leetcode/notes_leetcode/NotesLeetcodeApplication.java
@@ -0,0 +1,13 @@
+package notes.leetcode.notes_leetcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesLeetcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesLeetcodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/notes/leetcode/notes_leetcode/NotesLeetcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package notes.leetcode.notes_leetcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesLeetcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
